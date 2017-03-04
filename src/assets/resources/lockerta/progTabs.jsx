71dd12df14b826fa2746a36617923da04e92e35e
Ray Curran <rpcurr@gmail.com>
2017-03-04 09:19:25 -0600

    Set up basic tab switching and list showing up on the program-reviews tab

--- src/assets/resources/lockerta/progTabs.jsx
+++ src/assets/resources/lockerta/progTabs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import InformationTabContainer from '../../../components/ProgramDetail/InformationTab/InformationTabContainer.jsx'; //eslint-disable-line import/extensions
 import SchedulingTabContainer from '../../../components/SchoolDetail/SchedulingTab/SchedulingTabContainer.jsx';
-import IntegrationsTabContainer from '../../../components/SchoolDetail/IntegrationsTab/IntegrationsTabContainer.jsx'; //eslint-disable-line import/extensions
+import ProgramReviewsTabContainer from '../../../components/ProgramDetail/ProgramReviewsTab/ProgramReviewsTabContainer.jsx'; //eslint-disable-line import/extensions
 
 export const tabs = [
     {
@@ -15,10 +15,10 @@ export const tabs = [
     },
     {
         tabID: 'integrations',
-        label: 'Integrations',
+        label: 'Reviews',
         tabIndex: 1,
         generateComponent(props) {
-            return <IntegrationsTabContainer {...props} />;
+            return <ProgramReviewsTabContainer {...props} />;
         }
     }
 ];
